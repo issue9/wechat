7c4c51274bb597b7d969363ce58bb23e676e2f9d
caixw <xnotepad@gmail.com>
2016-10-20 02:33:16 +0800

    调整目录结构，将支付包也添加到当前包中

---
+++ mp/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// 微信公众号的相关接口
+package mp
---
+++ pay/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// 微信支付的相关接口
+package pay
