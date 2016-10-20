ae1b5b3e347f0fd8ccba35c56f7a1cb2114d97bb
caixw <xnotepad@gmail.com>
2016-10-20 22:18:43 +0800

    添加一个 common 包，用于处理所有包的公共内容

---
+++ common/cdata_test.go
@@ -0,0 +1,41 @@
+// Copyright 2016 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package common
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/issue9/assert"
+)
+
+type CDataTester struct {
+	XMLName xml.Name `xml:"xml"`
+	Data1   CData    `xml:"data1"`
+}
+
+func TestMarshalCData(t *testing.T) {
+	a := assert.New(t)
+
+	obj := &CDataTester{
+		Data1: CData{"abc<def"},
+	}
+
+	bs, err := xml.Marshal(obj)
+	a.NotError(err).Equal(string(bs), `<xml><data1><![CDATA[abc<def]]></data1></xml>`)
+
+}
+
+func TestUnmarshalCData(t *testing.T) {
+	a := assert.New(t)
+
+	data := []byte(`
+	<xml>
+	<data1><![CDATA[adf<ddd]]></data1>
+	</xml>
+	`)
+	o := &CDataTester{}
+	a.NotError(xml.Unmarshal(data, o)).Equal(o.Data1.Text, "adf<ddd")
+}
--- mp/message/reply.go
+++ mp/message/reply.go
@@ -6,46 +6,30 @@ package message
 
 import (
 	"encoding/xml"
+
+	"github.com/issue9/wechat/common"
 )
 
 var ReplySuccess = []byte("success")
 
 // ReplyTransferCustomerService 转发消息
 type ReplyTransferCustomerService struct {
-	base
-}
-
-// ReplyText 回复文本消息
-type ReplyText struct {
-	Text
+	ToUserName   common.CData `xml:"ToUserName"`   // 开发者微信号
+	FromUserName common.CData `xml:"FromUserName"` // 发送方帐号（一个 OpenID）
+	MsgType      common.CData `xml:"MsgType"`      // 消息类型
+	CreateTime   int64        `xml:"CreateTime"`   // 消息创建时间 （整型）
 }
 
 // NewReplyTranferCustomerService 将所有的消息进行转发
 func NewReplyTranferCustomerService(m Messager) *ReplyTransferCustomerService {
-	return &ReplyTransferCustomerService{base: base{
-		ToUserName:   m.From(),
-		FromUserName: m.To(),
-		MsgType:      m.Type(),
+	return &ReplyTransferCustomerService{
+		ToUserName:   common.CData{m.From()},
+		FromUserName: common.CData{m.To()},
+		MsgType:      common.CData{m.Type()},
 		CreateTime:   m.Created(),
-	}}
+	}
 }
 
 func (t *ReplyTransferCustomerService) Bytes() ([]byte, error) {
-	return xml.Marshal(t)
-}
-
-// NewReplyText 从文本消息中构建一个回复文本消息
-func NewReplyText(t *Text) *ReplyText {
-	ret := &ReplyText{}
-	ret.ToUserName = t.From()
-	ret.FromUserName = t.To()
-	ret.MsgType = t.Type()
-	ret.CreateTime = t.Created()
-	ret.Content = t.Content
-
-	return ret
-}
-
-func (t *ReplyText) Bytes() ([]byte, error) {
 	return xml.Marshal(t)
 }
